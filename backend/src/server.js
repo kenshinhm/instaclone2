c75faebed8c060590b60a20f71b46cd4e9a5b3c7
jake <kenshinhm@naver.com>
2019-05-19 19:29:47 +0900

    setting up the server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,15 @@
-require("dotenv").config();
 import {GraphQLServer} from "graphql-yoga";
+import schema from "src/schema.js";
+import logger from 'morgan';
+
+require("dotenv").config();
 
 const PORT = process.env.PORT || 4000;
 
-const typeDefs = `
-    type Query{
-        hello: String!
-    }
-`;
+const server = new GraphQLServer({schema});
 
-const resolvers = {
-    Query: {
-        hello: () => "Hi"
-    }
-};
-
-const server = new GraphQLServer({typeDefs, resolvers});
+//Express server can be touched from GraphQLServer
+//Using logger from morgan as a middleware
+server.express.use(logger("dev"));
 
 server.start({port: PORT}, () => console.log(`Server running on port http://localhost:${PORT}`));
