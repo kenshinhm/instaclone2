1ea40173bdbe4ca311289664a78686abf560fb40
jaehyoungyoo <kenshinhm@naver.com>
2019-09-09 23:34:14 +0900

    toggleLike, addComment

---
+++ src/api/like/toggleLike/toggleLike.js
@@ -0,0 +1,48 @@
+import {prisma} from "../../../../generated/prisma-client";
+import {isAuthenticated} from "../../../middlewares.js";
+
+export default {
+    Mutation: {
+        toggleLike: async (_, args, {request}) => {
+            isAuthenticated(request);
+            const {postId} = args;
+            const {user} = request;
+            const filterOption = {
+                AND: [
+                    {
+                        user: {
+                            id: user.id
+                        }
+                    },
+                    {
+                        post: {
+                            id: postId
+                        }
+                    }
+                ]
+            };
+            try {
+                const existingLike = await prisma.$exists.like(filterOption);
+                if (existingLike) {
+                    await prisma.deleteManyLikes(filterOption);
+                } else {
+                    await prisma.createLike({
+                        user: {
+                            connect: {
+                                id: user.id
+                            }
+                        },
+                        post: {
+                            connect: {
+                                id: postId
+                            }
+                        }
+                    });
+                }
+                return true;
+            } catch {
+                return false;
+            }
+        }
+    }
+};
