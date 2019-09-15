9decf5a33e32edd30da025a09b1ab2f423d20d9d
jaehyoungyoo <kenshinhm@naver.com>
2019-09-15 22:24:19 +0900

    seeFullPost refactoring

--- src/api/post/computed.js
+++ src/api/post/computed.js
@@ -1,16 +1,20 @@
-import {prisma} from "../../../generated/prisma-client";
+import { prisma } from "../../../generated/prisma-client";
 
 export default {
     Post: {
-        isLiked: (parent, _, {request}) => {
-            const {user} = request;
-            const {id: postId} = parent;
+        isLiked: (parent, _, { request }) => {
+            const { user } = request;
+            const { id: postId } = parent;
             return prisma.$exists.like({
-                AND: [
-                    {user: {id: user.id}},
-                    {post: {id: postId}}
-                ]
+                AND: [{ user: { id: user.id } }, { post: { id: postId } }]
             });
-        }
+        },
+        likeCount: parent =>
+            prisma
+                .likesConnection({
+                    where: { post: { id: parent.id } }
+                })
+                .aggregate()
+                .count()
     }
 };
