f39b6232e72a0e87cf122d465188241471ae2277
jaehyoungyoo <kenshinhm@naver.com>
2019-09-15 15:46:43 +0900

    editUser, seeUser

--- src/server.js
+++ src/server.js
@@ -5,12 +5,13 @@ import logger from "morgan";
 import passport from "passport";
 import "./passport.js";
 import {authenticateJwt} from "./passport.js";
+import {isAuthenticated} from "./middlewares.js";
 
 const PORT = process.env.PORT || 9999;
 
 const server = new GraphQLServer({
     schema,
-    context: ({request}) => ({request})
+    context: ({request}) => ({request, isAuthenticated})
 });
 
 //Express server can be touched from GraphQLServer
