5183d5c72a19688f915c830bf80251d6ca559db7
jaehyoungyoo <kenshinhm@naver.com>
2019-10-27 17:30:36 +0900

    uploading to s3

--- src/upload.js
+++ src/upload.js
@@ -1,9 +1,30 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
 
-const upload = multer({dest: "uploads/"});
+const s3 = new aws.S3({
+    accessKeyId: process.env.AWS_KEY,
+    secretAccessKey: process.env.AWS_SECRET,
+    region: "ap-northeast-2"
+});
+
+const upload = multer({
+    storage: multerS3({
+        s3,
+        acl: "public-read",
+        bucket: "instaclone2-yjh",
+        metadata: function (req, file, cb) {
+            cb(null, {fieldName: file.fieldname});
+        },
+        key: function (req, file, cb) {
+            cb(null, Date.now().toString());
+        }
+    })
+});
+
 export const uploadMiddleware = upload.single("file");
 
 export const uploadController = (req, res) => {
-    const {file: {path}} = req;
-    res.json({path});
+    const {file: {location}} = req;
+    res.json({location});
 };
